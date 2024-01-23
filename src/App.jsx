6d455579858ccb9d5d01e775867a5808ff7c5d04
ProfL3302 <ProfL3302@lab.aca>
2024-01-23 10:27:51 -0500

    Import inicial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import MemoriaPage from "./MemoriaPage"
+
+const App = () => {
+    return <div>
+        <MemoriaPage />
+    </div>
+}
+
+export default App
---
+++ src/MemoriaPage.jsx
@@ -0,0 +1,5 @@
+const MemoriaPage = () => {
+
+}
+
+export default MemoriaPage
